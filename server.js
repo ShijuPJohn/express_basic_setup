12f8ed3495b49949ce6c6fa5248b4a5903eab801
ShijuPJohn <shijupjohn4u@gmail.com>
2023-10-28 02:53:36 +0530

    initial commit. basic structure

---
+++ server.js
@@ -0,0 +1,7 @@
+const app = require('./app');
+const dotenv = require('dotenv')
+dotenv.config({path: './config.env'})
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`Listening to port ${PORT}`);
+})
